17e4fe66101b08ffc16d337f30b52f433f3e38f6
Soumya Ranjan Barik <soumyaranjanbarik390@gmail.com>
2021-01-16 20:48:56 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import { Route, withRouter, Switch } from 'react-router-dom';
 
-export default function App() {
+import { AuthContextProvider } from './context/AuthContext';
+import './App.css';
+import Home from './pages/Home/Home';
+import Auth from './pages/Auth/Auth';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Course from './pages/Course/Course';
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <AuthContextProvider>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/course/:id" component={Course} />
+      </Switch>
+    </AuthContextProvider>
   );
 }
+export default withRouter(App);
